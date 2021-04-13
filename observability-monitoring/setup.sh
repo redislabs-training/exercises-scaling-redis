8a0f3d0390ac8efad35cc92b05e78802e97e6405
Kurt Moeller <kurt@redislabs.com>
2021-04-13 07:57:01 -0700

    use admin instead of learn

--- observability-monitoring/setup.sh
+++ observability-monitoring/setup.sh
@@ -8,10 +8,10 @@ done
 printf ' success\n'
 
 printf '\nBootstrap: '
-docker exec -it observability-monitoring_redis_enterprise_monitoring_1 sh -c "/opt/redislabs/bin/rladmin cluster create name cluster.local username learn@redislabs.com password redis123"
+docker exec -it observability-monitoring_redis_enterprise_monitoring_1 sh -c "/opt/redislabs/bin/rladmin cluster create name cluster.local username admin@redis.com password redis123"
 
 
 printf '\nCreate DB\n'
-docker exec -it observability-monitoring_redis_enterprise_monitoring_1 sh -c "curl --output /dev/null --silent -X POST -H 'cache-control: no-cache' -H 'Content-type: application/json' -u learn@redislabs.com:redis123 -d '{ \"name\": \"db-1\", \"memory_size\": 52428800, \"sharding\": false, \"shards_count\": 1, \"type\": \"redis\", \"proxy_policy\": \"single\", \"shards_placement\": \"dense\", \"port\": 12000}' -k https://localhost:9443/v1/bdbs"
+docker exec -it observability-monitoring_redis_enterprise_monitoring_1 sh -c "curl --output /dev/null --silent -X POST -H 'cache-control: no-cache' -H 'Content-type: application/json' -u admin@redis.com -d '{ \"name\": \"db-1\", \"memory_size\": 52428800, \"sharding\": false, \"shards_count\": 1, \"type\": \"redis\", \"proxy_policy\": \"single\", \"shards_placement\": \"dense\", \"port\": 12000}' -k https://localhost:9443/v1/bdbs"
 
 printf '\nGood to go!\n'
